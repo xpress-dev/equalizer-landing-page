9ec0fed33b15cc4cb49f39dcc0397fdf3eb5a150
Ali Akbarzadeh <akbarzadeh.ali77@gmail.com>
2023-04-07 20:11:38 +0430

    implemented hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import Hero from './components/Hero';
 const App = () => {
   return (
     <>
       <Header />
+      <main>
+        <Hero />
+      </main>
     </>
   );
 };
---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import classes from './Hero.module.css';
+const Hero = () => {
+  return (
+    <section className={classes.hero}>
+      <h1>We make your music sound extraordinary.</h1>
+      <p>
+        A system audio equalizer specifically designed for Android and iOS.
+        Freely tune the way your music sounds with a professional grade
+        parametric EQ & volume mixer. Control bass, mids, treble, gain control,
+        reverb, and more!
+      </p>
+    </section>
+  );
+};
+
+export default Hero;
